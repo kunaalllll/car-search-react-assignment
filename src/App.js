4b61c3a1f7e2d17a8af5e312b8326ac1b925d5ad
Kunal Mathur <kunalsrtk1404@gmail.com>
2023-10-04 19:44:02 +0530

    initiated the project and made the file structure

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        Assignment
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CardCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardCard = () => {
+  return (
+    <div>CardCard</div>
+  )
+}
+
+export default CardCard
